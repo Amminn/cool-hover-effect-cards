8985d255e62123b9f7e6d6b5b1e49f4c1423be7f
Amin <vrb.nnn@gmail.com>
2024-11-11 23:34:57 +0100

    feat: include rest of features, note js have two logics now

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-
-const handleOnMouseMove = e => {
+const handleOnMouseMove = (e) => {
   const { currentTarget: target } = e;
 
   console.log(target);
 
   const rect = target.getBoundingClientRect(),
     x = e.clientX - rect.left,
-    y = e.clientY - rect.top
+    y = e.clientY - rect.top;
 
   target.style.setProperty("--mouse-x", `${x}px`);
-  target.syle.setProperty("--mouse-y", `${y}px`);
-}
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
 
 for (const card of document.querySelectorAll(".card")) {
-  card.onmousemove = e => handleOnMouseMove(e);
+  card.onmousemove = (e) => handleOnMouseMove(e);
+}
+
+
+document.getElementById("cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
 }
